eadbf2c3faf89d4a0593f1d9398e9039e5dffca3
FreeProxyTelstra <free@telstra.uk>
2024-07-30 19:07:20 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'cefffd1d-cc58-4560-b4d1-44f1af528f30';
 
-const พร็อกซีไอพีs = ['cdn.xn--b6gac.eu.org'];
+const พร็อกซีไอพีs = ['cdn.anycast.eu.org'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
